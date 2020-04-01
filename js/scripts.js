e69db80b1fb68c078d7eaa615d87938d395cbc94
Ryan Duff <ryanduff613@gmail.com>
2020-04-01 15:45:10 -0700

    refactor code, all conditionals working, adds way to end of every word beginning with a vowel. need to work on adding "ay" to words beginning with y, moving first consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 var vowels = ['a','e','i','o','u'];
-var consonants = ['q','w','r','t','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
 
 var translate = function(userString){
-  if (userString.slice(0,1) === 'a' || userString.slice(0,1) === 'e' || userString.slice(0,1) === 'i' || userString.slice(0,1) === 'o' || userString.slice(0,1) === 'u') {
-  return userString + 'way';
- } else if (userString.slice(0,2) === 'qu') {
-     return 'qu printing' // userString -first letter+(first letter + 'ay')
- }  else if (userString.slice(0,1)=== 'y') {
-   return 'y working' // userString-y+ ("y"+'ay')
- }
+  if (userString.slice(0,1)=== 'y') {
+      return 'y conditional working' // userString-y+ ("y"+'ay')
+  } else if (userString.slice(0,2) === 'qu') {
+      return 'qu conditional working' // userString -first letter+(first letter + 'ay')
+  } else if (userString.slice(0,1) === 'a' || userString.slice(0,1) === 'e' || userString.slice(0,1) === 'i' || userString.slice(0,1) === 'o' || userString.slice(0,1) === 'u') {
+      return userString + 'way';
+  } else {
+      return 'consonant conditional working'
+ } 
 };
 
 $(document).ready(function() {
