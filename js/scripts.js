bed5cbf1954de5a35060870194db3a973451a16b
Ryan Duff <ryanduff613@gmail.com>
2020-04-01 14:51:48 -0700

    add conditional to 1st elseif, checks to see if word begins with aeiou, properly returns output

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,7 @@ var vowels = ['a','e','i','o','u'];
 var translate = function(userString){
  if (userString === 'a'|| userString === 'i') {
    return userString + "ay";
- } else if (userString === 'apart') {
-  console.log(zway.splice(0,1));
+ } else if (userString.slice(0,1) === 'a' || userString.slice(0,1) === 'e' || userString.slice(0,1) === 'i' || userString.slice(0,1) === 'o' || userString.slice(0,1) === 'u') {
   return userString + 'way';
  } else {
      return userString;
