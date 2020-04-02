ccf3e07bb0d2402c3290ae9abd1e92c7172b3adf
Ryan Duff <ryanduff613@gmail.com>
2020-04-01 19:41:35 -0700

    attempting to create function in business logic that returns the index of the first vowel in any word

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-
+// business logic
+var vowels = ['a','e','i','o','u'];
+//translate user input to pig latin
 var translate = function(userString){
   if (userString.slice(0,1)=== 'y') {
       return userString.slice(1)+userString.slice(0,1)+'ay'; 
@@ -7,28 +9,37 @@ var translate = function(userString){
   } else if (userString.slice(0,1) === 'a' || userString.slice(0,1) === 'e' || userString.slice(0,1) === 'i' || userString.slice(0,1) === 'o' || userString.slice(0,1) === 'u') {
       return userString + 'way';
   } else {
-      return userString.slice(userString.indexOf('a')) + userString.slice(0, userString.indexOf('a')) +'ay';
-      console.log(userString.slice(0,userString.indexOf('a')));
-      // return userString.slice(index of first vowell, indexoflastletter) + userString.slice(0, /*indexof firstvowel-1 */ +'ay'; 
-
-      // userstring.slice(indexOf(first vowel), indexof lastletter) + userstring.slice(0, -indexOf(first vowel-1- ) +'ay'
- } 
+      return userString.slice(userString.indexOf('a')) + userString.slice(0, userString.indexOf('a')) +'ay';  
+  } 
 };
+
+//return index of first vowel
+var test = function(userString){
+  var indexValues = [];
+  vowels.forEach(function(vowel){
+    indexValues.push((userString.indexOf(vowel)));
+  });
+  console.log(indexValues);
+
+  var filteredValues = indexValues.filter(function(indexValue){
+    return indexValue !== -1;
+  });
+
+  //return Math.min.apply(Math, filteredValues);
+
+  console.log(Math.min.apply(Math, filteredValues)); 
+};
+test('prize');
+
 
 $(document).ready(function() {
   $('#theForm').submit(function(event){
     event.preventDefault();
-    var userString = $('#userInput').val();
-    var outputString = translate(userString);
+    var userInput = $('#userInput').val();
+    var output = translate(userInput);
     //console.log(userString);
     //console.log(outputString);
 
-  $('#result').text(outputString).show();
+  $('#result').text(output).show();
   });
-
 });
-
-/*vowels.forEach(function(vowel){
-  return vowel;
-});
-*/ 
